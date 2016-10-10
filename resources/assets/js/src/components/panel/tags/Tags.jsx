5abd914cf443c42aa14c5735d03c338881958a47
Mehrdad Shokri <shokri_m@hotmail.com>
2016-10-10 18:51:00 +0330

    Create table

--- resources/assets/js/src/components/panel/tags/Tags.jsx
+++ resources/assets/js/src/components/panel/tags/Tags.jsx
@@ -1,8 +1,38 @@
 import React, {Component} from 'react';
-import {Table, TableBody, TableRow, TableRowColumn, TableHeader, TableHeaderColumn} from 'material-ui';
+import {Table, TableBody, TableRow, TableRowColumn, TableHeader, TableHeaderColumn, TableFooter} from 'material-ui';
 import styles from '../../../../../sass/components/panel/tags/Tags.scss'
 const classNames = require('classnames/bind');
 const cx = classNames.bind(styles);
+const tableData = [
+		{
+				name: 'John Smith',
+				level: 1,
+		},
+		{
+				name: 'Randal White',
+				level: 1,
+		},
+		{
+				name: 'Stephanie Sanders',
+				level: 'Employed',
+		},
+		{
+				name: 'Steve Brown',
+				level: 'Employed',
+		},
+		{
+				name: 'Joyce Whitten',
+				level: 'Employed',
+		},
+		{
+				name: 'Samuel Roberts',
+				level: 'Employed',
+		},
+		{
+				name: 'Adam Moore',
+				level: 'Employed',
+		},
+];
 
 class Tags extends Component {
 		constructor(props) {
@@ -32,10 +62,28 @@ class Tags extends Component {
 										<input type="text" value={this.state.currentTag} onChange={this.handleInputChange}
 													 className={cx("tagSelect")}/>
 								</form>
-								<Table>
-										<TableHeader>
-
+								<Table selectable={false}>
+										<TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+												<TableRow >
+														<TableHeaderColumn tooltip="اسم تگ"
+																							 tooltipStyle={{fontSize: 14}}
+																							 style={{fontSize: 20}}>نام
+														</TableHeaderColumn>
+														<TableHeaderColumn tooltip="سطح کمتر، اولویت بیشتری در جستجو دارد"
+																							 tooltipStyle={{fontSize: 14}}
+																							 style={{fontSize: 20}}>سطح
+														</TableHeaderColumn>
+												</TableRow>
 										</TableHeader>
+										<TableBody
+												displayRowCheckbox={false}>
+												{tableData.map((row, index) => (
+														<TableRow key={index} selected={row.selected}>
+																<TableRowColumn style={{fontSize: 14}}>{row.name}</TableRowColumn>
+																<TableRowColumn style={{fontSize: 14}}>{row.level}</TableRowColumn>
+														</TableRow>
+												))}
+										</TableBody>
 								</Table>
 						</div>
 				)
