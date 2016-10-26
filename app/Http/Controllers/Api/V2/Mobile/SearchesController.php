18433dd20daa64fd55d9bb9f882cbaac28c12af7
Mehrdad Shokri <shokri_m@hotmail.com>
2016-10-26 08:21:15 +0330

    Searching with all conditions

--- app/Http/Controllers/Api/V2/Mobile/SearchesController.php
+++ app/Http/Controllers/Api/V2/Mobile/SearchesController.php
@@ -8,69 +8,84 @@ use App\Http\Controllers\Api\v2\BaseController;
 use App\Location;
 use app\Repository\CityRepository;
 use app\Repository\SearchRepository;
+use App\Tag;
 use App\Venue;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class SearchesController extends BaseController
-{
+class SearchesController extends BaseController {
 
-    /**
-     * @var SearchRepository
-     */
-    private $repository;
-    /**
-     * @var CityRepository
-     */
-    private $cityRepository;
+	/**
+	 * @var SearchRepository
+	 */
+	private $repository;
+	/**
+	 * @var CityRepository
+	 */
+	private $cityRepository;
 
-    public function __construct(SearchRepository $repository, CityRepository $cityRepository)
-    {
-        $this->repository = $repository;
-        $this->cityRepository = $cityRepository;
-    }
+	public function __construct(SearchRepository $repository, CityRepository $cityRepository)
+	{
+		$this->repository = $repository;
+		$this->cityRepository = $cityRepository;
+	}
 
-    public function searchVenues(Request $request)
-    {
-        $lat = $request->get('lat');
-        $lng = $request->get('lng');
-        $rules = [
-            'lat' => 'required|numeric',
-            'lng' => 'required|numeric',
-            'streetName' => 'string'
-        ];
-        $validator = app('validator')->make(compact('lat', 'lng'), $rules);
-        if ($validator->fails())
-            $this->response->errorBadRequest("Validation failed.");
-        $userCity = $this->cityRepository->getCity($lat, $lng);
-		dd($userCity);
-        $streetIds = $this->repository->searchStreet($request->get('name'), $userCity)->pluck('OGR_FID');
-        $venueIds = collect();
-        foreach ($streetIds as $id) {
-            $locations = DB::table(DB::raw("locations,streets"))->select(array('venue_id'))->where('OGR_FID', $id)->whereRaw("st_within(geolocation,st_envelope(shape))")->get();
-            $locations->each(function ($item) use ($venueIds) {
-                $venueIds->push($item);
-            });
-        }
-        $venues = Venue::whereIn('id', $venueIds->unique()->pluck('venue_id'))->get();
-        dd($venues);
-        return $this->repository->searchStreet($request->get('name'), $userCity);
-    }
+	public function searchVenues(Request $request)
+	{
+		$lat = $request->get('lat');
+		$lng = $request->get('lng');
+		$rules = [
+			'lat' => 'required|numeric',
+			'lng' => 'required|numeric',
+			'streetName' => 'string',
+			'query' => 'string'
+		];
+		$validator = app('validator')->make(compact('lat', 'lng'), $rules);
+		if ($validator->fails())
+			$this->response->errorBadRequest("Validation failed.");
+		$userCity = $this->cityRepository->getCity($lat, $lng);
+		if ($request->get('streetName'))  //find in that place
+		{
+			$streetIds = $this->repository->searchStreet($request->get('streetName'), $userCity)->pluck('OGR_FID');
+			$venueIds = collect();
+			foreach ($streetIds as $id)
+			{
+				$locations = DB::table(DB::raw("locations,streets"))->where('OGR_FID', $id)->whereRaw("ST_DISTANCE(geolocation,shape)*100000 as distance < 200")->get();
+				$locations->each(function ($item) use ($venueIds)
+				{
+					$venueIds->push($item->venue_id);
+				});
+				dd($locations);
+			}
+			dd($this->repository->suggestStreet($request->get('streetName'),$userCity));
+			if (!$request->get('query'))
+			{
+				return $this->repository->searchStreet($request->get('name'), $userCity);
+			}
+			$venues = $this->repository->suggestVenue($request->get('query'));
+
+		}
+		else
+		{
+//			find nearby places
+		}
+
+	}
 
 
-    public function suggestStreets(Request $request)
-    {
-        $lat = $request->get('lat');
-        $lng = $request->get('lng');
-        $rules = [
-            'lat' => 'required|numeric',
-            'lng' => 'required|numeric',
-            'name' => 'string'
-        ];
-        $validator = app('validator')->make(compact('lat', 'lng'), $rules);
-        if ($validator->fails())
-            $this->response->errorBadRequest("Validation failed.");
-        $userCity = $this->cityRepository->getCity($lat, $lng);
-        return $this->response->collection($this->repository->suggestStreet($request->get('name'), $userCity), new StreetTransformer());
-    }
+	public function suggestStreets(Request $request)
+	{
+		$lat = $request->get('lat');
+		$lng = $request->get('lng');
+		$rules = [
+			'lat' => 'required|numeric',
+			'lng' => 'required|numeric',
+			'name' => 'string'
+		];
+		$validator = app('validator')->make(compact('lat', 'lng'), $rules);
+		if ($validator->fails())
+			$this->response->errorBadRequest("Validation failed.");
+		$userCity = $this->cityRepository->getCity($lat, $lng);
+		return $this->response->collection($this->repository->suggestStreet($request->get('name'), $userCity), new StreetTransformer());
+	}
 }
