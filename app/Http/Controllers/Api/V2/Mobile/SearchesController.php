f7b8c354db0492c0a75e1c6f88e8b601622889ef
Mehrdad Shokri <shokri_m@hotmail.com>
2016-10-26 11:58:46 +0330

    Searching in places

--- app/Http/Controllers/Api/V2/Mobile/SearchesController.php
+++ app/Http/Controllers/Api/V2/Mobile/SearchesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V2\Mobile;
 
 use App\Api\Transformer\StreetTransformer;
+use App\Api\Transformer\VenueTransformer;
 use App\City;
 use App\Http\Controllers\Api\v2\BaseController;
 use App\Location;
@@ -10,6 +11,7 @@ use app\Repository\CityRepository;
 use app\Repository\SearchRepository;
 use App\Tag;
 use App\Venue;
+use Hamcrest\Collection\IsTraversableWithSizeTest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -44,30 +46,51 @@ class SearchesController extends BaseController {
 		if ($validator->fails())
 			$this->response->errorBadRequest("Validation failed.");
 		$userCity = $this->cityRepository->getCity($lat, $lng);
-		if ($request->get('streetName'))  //find in that place
+		if ($request->get('streetName'))
 		{
 			$streetIds = $this->repository->searchStreet($request->get('streetName'), $userCity)->pluck('OGR_FID');
-			$venueIds = collect();
+			$streetVenueIds = collect();
 			foreach ($streetIds as $id)
 			{
-				$locations = DB::table(DB::raw("locations,streets"))->where('OGR_FID', $id)->whereRaw("ST_DISTANCE(geolocation,shape)*100000 as distance < 200")->get();
-				$locations->each(function ($item) use ($venueIds)
+				$locations = DB::table(DB::raw("locations,streets"))->select(DB::raw("ST_DISTANCE(geolocation,shape)*100000 as distance , venue_id"))->where('OGR_FID', $id)->orderBy('distance', 'asc')->havingRaw("distance < 200")->get();
+				$locations->each(function ($item) use ($streetVenueIds)
 				{
-					$venueIds->push($item->venue_id);
+					$streetVenueIds->push($item->venue_id);
 				});
-				dd($locations);
 			}
-			dd($this->repository->suggestStreet($request->get('streetName'),$userCity));
+			$streetVenueIds = $streetVenueIds->unique();
 			if (!$request->get('query'))
 			{
-				return $this->repository->searchStreet($request->get('name'), $userCity);
+				if ($streetVenueIds->count() == 0)
+				{
+					return $this->response->collection(collect(), new VenueTransformer());
+				}
+				$venueIdsArray = $streetVenueIds->all();
+				$idsImploded = implode(',', $venueIdsArray);
+				$venues = Venue::whereIn('id', $venueIdsArray)->orderByRaw("field(id,{$idsImploded})", $venueIdsArray)->get();
+				return $this->response->collection($venues, new VenueTransformer());
+			}
+			else
+			{
+				$queryVenueIds = $this->repository->suggestVenue($request->get('query'))->pluck('id');
+				$ids = $queryVenueIds->intersect($streetVenueIds)->all();
+				$idsImploded = implode(',', $ids);
+				$venues = Venue::whereIn('id', $ids)->orderByRaw("field(id,{$idsImploded})", $ids)->get();
+				return $this->response->collection($venues, new VenueTransformer());
 			}
 			$venues = $this->repository->suggestVenue($request->get('query'));
-
 		}
 		else
 		{
-//			find nearby places
+			//			find nearby places
+			if ($request->get('query'))
+			{
+
+			}
+			else
+			{
+
+			}
 		}
 
 	}
